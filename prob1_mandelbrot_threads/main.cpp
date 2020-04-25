42d78179220cd627007db47881a9ba3a423d5fad
xmeng <mxf3306@gmail.com>
2020-04-25 21:19:24 +0100

    style fixt

--- prob1_mandelbrot_threads/main.cpp
+++ prob1_mandelbrot_threads/main.cpp
@@ -19,15 +19,14 @@ extern void mandelbrotThread(
     int output[]);
 
 extern void writePPMImage(
-    int* data,
+    int *data,
     int width, int height,
     const char *filename,
     int maxIterations);
 
-void
-scaleAndShift(float& x0, float& x1, float& y0, float& y1,
-              float scale,
-              float shiftX, float shiftY)
+void scaleAndShift(float &x0, float &x1, float &y0, float &y1,
+                   float scale,
+                   float shiftX, float shiftY)
 {
 
     x0 *= scale;
@@ -38,10 +37,10 @@ scaleAndShift(float& x0, float& x1, float& y0, float& y1,
     x1 += shiftX;
     y0 += shiftY;
     y1 += shiftY;
-
 }
 
-void usage(const char* progname) {
+void usage(const char *progname)
+{
     printf("Usage: %s [options]\n", progname);
     printf("Program Options:\n");
     printf("  -t  --threads <N>       Use N threads\n");
@@ -51,21 +50,26 @@ void usage(const char* progname) {
     printf("  -?  --help              This message\n");
 }
 
-bool verifyResult (int *gold, int *result, int width, int height) {
+bool verifyResult(int *gold, int *result, int width, int height)
+{
     int i, j;
     int errLimit = 5;
     bool ok = true;
 
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < width; j++) {
-            if (gold[i * width + j] != result[i * width + j]) {
-                printf ("Mismatch : [%d][%d], Expected : %d, Actual : %d\n",
-                            i, j, gold[i * width + j], result[i * width + j]);
-		ok = false;
-		if (--errLimit <= 0) {
-		    printf(" ...\n");
-		    return ok;
-		}
+    for (i = 0; i < height; i++)
+    {
+        for (j = 0; j < width; j++)
+        {
+            if (gold[i * width + j] != result[i * width + j])
+            {
+                printf("Mismatch : [%d][%d], Expected : %d, Actual : %d\n",
+                       i, j, gold[i * width + j], result[i * width + j]);
+                ok = false;
+                if (--errLimit <= 0)
+                {
+                    printf(" ...\n");
+                    return ok;
+                }
             }
         }
     }
@@ -75,7 +79,8 @@ bool verifyResult (int *gold, int *result, int width, int height) {
 
 #define VIEWCNT 7
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
     const int width = 1250;
     // Height of the canvas
@@ -86,32 +91,33 @@ int main(int argc, char** argv) {
     int numThreads = 2;
 
     float x0 = -2.167;
-    float x1 =  1.167;
+    float x1 = 1.167;
     float y0 = -1;
     float y1 = 1;
 
     // Support VIEWCNT views
-    float scaleValues[VIEWCNT] = { 0.01f, 1.0f, 0.015f, 0.02f, 0.02f, 0.02f,  0.002f };
-    float shiftXs[VIEWCNT]      = { 0.0f, 0.0f, -0.98f, 0.35f, 0.0f, -1.5f,  -1.4f };
-    float shiftYs[VIEWCNT]      = { 0.0f, 0.0f,  0.30f, 0.05f,  0.73f, 0.0f, 0.0f };
+    float scaleValues[VIEWCNT] = {0.01f, 1.0f, 0.015f, 0.02f, 0.02f, 0.02f, 0.002f};
+    float shiftXs[VIEWCNT] = {0.0f, 0.0f, -0.98f, 0.35f, 0.0f, -1.5f, -1.4f};
+    float shiftYs[VIEWCNT] = {0.0f, 0.0f, 0.30f, 0.05f, 0.73f, 0.0f, 0.0f};
 
     // parse commandline options ////////////////////////////////////////////
     int opt;
     static struct option long_options[] = {
         {"threads", 1, 0, 't'},
         {"view", 1, 0, 'v'},
-	{"field", 1, 0, 'f'},
-	{"out", 1, 0, 'o'},
+        {"field", 1, 0, 'f'},
+        {"out", 1, 0, 'o'},
         {"help", 0, 0, '?'},
-        {0 ,0, 0, 0}
-    };
+        {0, 0, 0, 0}};
 
     int viewIndex = 0;
     char fname[256];
     bool have_file = false;
-    while ((opt = getopt_long(argc, argv, "t:v:f:o:?", long_options, NULL)) != EOF) {
+    while ((opt = getopt_long(argc, argv, "t:v:f:o:?", long_options, NULL)) != EOF)
+    {
 
-        switch (opt) {
+        switch (opt)
+        {
         case 't':
         {
             numThreads = atoi(optarg);
@@ -121,26 +127,28 @@ int main(int argc, char** argv) {
         {
             viewIndex = atoi(optarg);
             // change view settings
-            if (viewIndex < 0 || viewIndex >= VIEWCNT)  {
+            if (viewIndex < 0 || viewIndex >= VIEWCNT)
+            {
                 fprintf(stderr, "Invalid view index %d\n", viewIndex);
                 return 1;
             }
             break;
         }
-	case 'f':
-	{
-	    if (sscanf(optarg, "%f:%f:%f:%f", &x0, &y0, &x1, &y1) != 4) {
-		fprintf(stderr, "Couldn't extract field from '%s'\n", optarg);
-		exit(1);
-	    }
-	    break;
-	}
-	case 'o':
-	{
-	    strcpy(fname, optarg);
-	    have_file = true;
-	    break;
-	}
+        case 'f':
+        {
+            if (sscanf(optarg, "%f:%f:%f:%f", &x0, &y0, &x1, &y1) != 4)
+            {
+                fprintf(stderr, "Couldn't extract field from '%s'\n", optarg);
+                exit(1);
+            }
+            break;
+        }
+        case 'o':
+        {
+            strcpy(fname, optarg);
+            have_file = true;
+            break;
+        }
         case '?':
         default:
             usage(argv[0]);
@@ -153,18 +161,18 @@ int main(int argc, char** argv) {
     float shiftY = shiftYs[viewIndex];
     scaleAndShift(x0, x1, y0, y1, scaleValue, shiftX, shiftY);
 
-    if (have_file) {
-	// In this mode, assume goal is to simply generate the output as fast as possible
-	int* output = new int[width*height];
+    if (have_file)
+    {
+        // In this mode, assume goal is to simply generate the output as fast as possible
+        int *output = new int[width * height];
         mandelbrotThread(numThreads, x0, y0, x1, y1, width, iheight, maxIterations, output);
-	writePPMImage(output, width, height, fname, maxIterations);
-	delete [] output;
-	exit(0);
+        writePPMImage(output, width, height, fname, maxIterations);
+        delete[] output;
+        exit(0);
     }
 
-
-    int* output_serial = new int[width*height];
-    int* output_thread = new int[width*height];
+    int *output_serial = new int[width * height];
+    int *output_thread = new int[width * height];
 
     //
     // Run the serial implementation.  Run the code numRuns times and
@@ -173,7 +181,8 @@ int main(int argc, char** argv) {
     int numRuns = 3;
     memset(output_serial, 0, width * height * sizeof(int));
     double minSerial = 1e30;
-    for (int i = 0; i < numRuns; ++i) {
+    for (int i = 0; i < numRuns; ++i)
+    {
         double startTime = CycleTimer::currentSeconds();
         mandelbrotSerial(x0, y0, x1, y1, width, iheight, 0, iheight, maxIterations, output_serial);
         double endTime = CycleTimer::currentSeconds();
@@ -189,7 +198,8 @@ int main(int argc, char** argv) {
     //
     memset(output_thread, 0, width * height * sizeof(int));
     double minThread = 1e30;
-    for (int i = 0; i < numRuns; ++i) {
+    for (int i = 0; i < numRuns; ++i)
+    {
         double startTime = CycleTimer::currentSeconds();
         mandelbrotThread(numThreads, x0, y0, x1, y1, width, iheight, maxIterations, output_thread);
         double endTime = CycleTimer::currentSeconds();
@@ -200,8 +210,9 @@ int main(int argc, char** argv) {
     sprintf(fname, "mandelbrot-v%d-thread-%d.ppm", viewIndex, numThreads);
     writePPMImage(output_thread, width, height, fname, maxIterations);
 
-    if (! verifyResult (output_serial, output_thread, width, height)) {
-        printf ("ERROR : Output from threads does not match serial output\n");
+    if (!verifyResult(output_serial, output_thread, width, height))
+    {
+        printf("ERROR : Output from threads does not match serial output\n");
 
         delete[] output_serial;
         delete[] output_thread;
@@ -210,7 +221,7 @@ int main(int argc, char** argv) {
     }
 
     // compute speedup
-    printf("++++\t\t\t\t(%.2fx speedup from %d threads)\n", minSerial/minThread, numThreads);
+    printf("++++\t\t\t\t(%.2fx speedup from %d threads)\n", minSerial / minThread, numThreads);
 
     delete[] output_serial;
     delete[] output_thread;
--- prob1_mandelbrot_threads/mandelbrot.cpp
+++ prob1_mandelbrot_threads/mandelbrot.cpp
@@ -42,19 +42,19 @@
   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-
 // Core computation of Mandelbrot set membershop
 // Iterate complex number c to determine whether it diverges
 static inline int mandel(float c_re, float c_im, int count)
 {
     float z_re = c_re, z_im = c_im;
     int i;
-    for (i = 0; i < count; ++i) {
+    for (i = 0; i < count; ++i)
+    {
 
         if (z_re * z_re + z_im * z_im > 4.f)
             break;
 
-        float new_re = z_re*z_re - z_im*z_im;
+        float new_re = z_re * z_re - z_im * z_im;
         float new_im = 2.f * z_re * z_im;
         z_re = c_re + new_re;
         z_im = c_im + new_im;
@@ -75,19 +75,21 @@ static inline int mandel(float c_re, float c_im, int count)
 // * width, height describe the size of the output image
 // * startRow, totalRows describe how much of the image to compute
 void mandelbrotSerial(
-		      float x0, float y0, float x1, float y1,
-		      int width, int height,
-		      int startRow, int totalRows,
-		      int maxIterations,
-		      int output[])
+    float x0, float y0, float x1, float y1,
+    int width, int height,
+    int startRow, int totalRows,
+    int maxIterations,
+    int output[])
 {
     float dx = (x1 - x0) / width;
     float dy = (y1 - y0) / height;
 
     int endRow = startRow + totalRows;
 
-    for (int j = startRow; j < endRow; j++) {
-        for (int i = 0; i < width; ++i) {
+    for (int j = startRow; j < endRow; j++)
+    {
+        for (int i = 0; i < width; ++i)
+        {
             float x = x0 + i * dx;
             float y = y0 + j * dy;
 
@@ -97,15 +99,15 @@ void mandelbrotSerial(
     }
 }
 
-
 // Threading support
-typedef struct {
+typedef struct
+{
     float x0, x1;
     float y0, y1;
     int width;
     int height;
     int maxIterations;
-    int* output;
+    int *output;
     int threadId;
     int numThreads;
 } WorkerArgs;
@@ -114,9 +116,10 @@ typedef struct {
 // workerThreadStart --
 //
 // Thread entrypoint.
-void* workerThreadStart(void* threadArgs) {
+void *workerThreadStart(void *threadArgs)
+{
 
-    WorkerArgs* args = static_cast<WorkerArgs*>(threadArgs);
+    WorkerArgs *args = static_cast<WorkerArgs *>(threadArgs);
 
     // TODO: Implement worker thread here.
     return NULL;
@@ -128,37 +131,38 @@ void* workerThreadStart(void* threadArgs) {
 // Multi-threaded implementation of mandelbrot set image generation.
 // Multi-threading performed via pthreads.
 void mandelbrotThread(
-		      int numThreads,
-		      float x0, float y0, float x1, float y1,
-		      int width, int height,
-		      int maxIterations, int output[])
+    int numThreads,
+    float x0, float y0, float x1, float y1,
+    int width, int height,
+    int maxIterations, int output[])
 {
     const static int MAX_THREADS = 32;
 
     if (numThreads > MAX_THREADS)
-	{
-	    fprintf(stderr, "Error: Max allowed threads is %d\n", MAX_THREADS);
-	    exit(1);
-	}
+    {
+        fprintf(stderr, "Error: Max allowed threads is %d\n", MAX_THREADS);
+        exit(1);
+    }
 
     pthread_t workers[MAX_THREADS];
     WorkerArgs args[MAX_THREADS];
 
-    for (int i=0; i<numThreads; i++) {
+    for (int i = 0; i < numThreads; i++)
+    {
         args[i].threadId = i;
-	// TODO: Set thread arguments here
+        // TODO: Set thread arguments here
     }
 
     // Fire up the worker threads.  Note that numThreads-1 pthreads
     // are created and the main app thread is used as a worker as
     // well.
 
-    for (int i=1; i<numThreads; i++)
+    for (int i = 1; i < numThreads; i++)
         pthread_create(&workers[i], NULL, workerThreadStart, &args[i]);
 
     workerThreadStart(&args[0]);
 
     // wait for worker threads to complete
-    for (int i=1; i<numThreads; i++)
+    for (int i = 1; i < numThreads; i++)
         pthread_join(workers[i], NULL);
 }
