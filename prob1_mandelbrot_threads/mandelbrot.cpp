cd46a58601ed5c07bcd3543bfd32fb91ae6ddfd9
xmeng <mxf3306@gmail.com>
2020-04-25 22:26:09 +0100

    Simple assignment

--- prob1_mandelbrot_threads/mandelbrot.cpp
+++ prob1_mandelbrot_threads/mandelbrot.cpp
@@ -122,6 +122,20 @@ void *workerThreadStart(void *threadArgs)
     WorkerArgs *args = static_cast<WorkerArgs *>(threadArgs);
 
     // TODO: Implement worker thread here.
+    int height = args->height;
+    int numRow = height / args->numThreads;
+    int leftOver = height % args->numThreads;
+    int threadId = args->threadId;
+
+    if (threadId < leftOver) {
+        int startRow = (numRow + 1) * threadId;
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1,
+                         args->width, args->height, startRow, numRow+1, args->maxIterations, args->output);
+    } else {
+        int startRow = (numRow + 1) * leftOver + numRow * (threadId - leftOver);
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1,
+                         args->width, args->height, startRow, numRow, args->maxIterations, args->output);
+    }
     return NULL;
 }
 
@@ -151,6 +165,15 @@ void mandelbrotThread(
     {
         args[i].threadId = i;
         // TODO: Set thread arguments here
+        args[i].x0 = x0;
+        args[i].y0 = y0;
+        args[i].x1 = x1;
+        args[i].y1 = y1;
+        args[i].width = width;
+        args[i].height = height;
+        args[i].maxIterations = maxIterations;
+        args[i].output = output;
+        args[i].numThreads = numThreads;
     }
 
     // Fire up the worker threads.  Note that numThreads-1 pthreads
